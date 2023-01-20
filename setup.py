5e6d33f29645a702edc274df5d1d4549f91038b7
Ryukkun <tkytwins2@gmail.com>
2023-01-20 10:40:14 +0900

    del

--- setup.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import os
-
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-try:
-    subprocess.run(['pip','install','-r','requirements.txt'])
-except Exception:
-    subprocess.run(['pip3','install','-r','requirements.txt'])
