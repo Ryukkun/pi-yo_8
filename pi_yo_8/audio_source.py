240f7aeb7c756cda47e476a2ad94ed1511a52ebc
Ryukkun <tkytwins2@gmail.com>
2023-03-13 15:17:52 +0900

    log level

--- pi_yo_8/audio_source.py
+++ pi_yo_8/audio_source.py
@@ -21,7 +21,25 @@ re_URL_PL_Video = re.compile(r'https://(www.|)youtube.com/watch\?v=(.+)&list=(.+
 re_URL_PL = re.compile(r'https://(www.|)youtube.com/playlist\?list=')
 re_URL = re.compile(r'http')
 
-
+"""
+The MIT License (MIT)
+Copyright (c) 2015-present Rapptz
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
 class FFmpegPCMAudio(FFmpegAudio):
     """An audio source from FFmpeg (or AVConv).
     This launches a sub-process to a specific input file given.
@@ -271,7 +289,7 @@ class StreamAudioData:
     async def AudioSource(self, opus:bool, sec:int=0, speed:float=1.0, pitch:int=0):
         FFMPEG_OPTIONS = {
             'before_options': '',
-            'options': f'-vn -application lowdelay'
+            'options': f'-vn -application lowdelay -loglevel quiet'
             }
         af = []
 
