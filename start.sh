4eada9808794e640accf8b7079444a17f206201f
Ryukkun_ <tkytwins2@gmail.com>
2026-08-01 23:03:30 +0900

    bashに統一

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 cd "$(dirname "${BASH_SOURCE[0]:-$0}")"
 git pull
 mkdir -p logs
 uv sync --upgrade
 
-screen -L -Logfile "./logs/screen-$(date +%Y%m%d_%H%M%S).log" -dmS pi-yo_8 uv run main.py 
+screen -L -Logfile "./logs/screen-$(date +%Y%m%d_%H%M%S).log" -dmS pi-yo8 uv run main.py 
