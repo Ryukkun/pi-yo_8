40cdc34138d0c9e6b2621d4e81b1c5adc810434c
Ryukkun_ <tkytwins2@gmail.com>
2026-08-01 22:19:25 +0900

    screenでlogを生成するように

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 cd "$(dirname "${BASH_SOURCE[0]:-$0}")"
 git pull
+mkdir -p logs
 uv sync --upgrade
-screen uv run main.py 
+
+screen -L -Logfile "./logs/screen-$(date +%Y%m%d_%H%M%S).log" -dmS pi-yo_8 uv run main.py 
